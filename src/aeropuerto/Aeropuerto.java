eee68743d6362566314210febeb8c1b686de43de
catalina <catalina.pineda@mariaelvinia.edu.co>
2020-09-26 17:38:08 -0500

    Versión 1.0
    Creacion de la clase aeropuerto, de sus metodos Get y Set, método constructor y de dos métodos adicionales. Se instanciaron dos objetos a partir de una clase.

--- src/aeropuerto/Aeropuerto.java
+++ src/aeropuerto/Aeropuerto.java
@@ -1,21 +1,115 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aeropuerto;
 
-/**
- *
- * @author catal
- */
-public class Aeropuerto {
+import java.util.Date;
 
+
+public class Aeropuerto {
+    
+    private String nombre;
+    private String ciudadUbicacion;
+    private int cantidadDeVuelosAlDia;
+    private Date fechaDeApertura;
+    private boolean vuelosVip;
+
+    public Aeropuerto(String nombre, 
+            String ciudadUbicacion, 
+            int cantidadDeVuelosAlDia,
+            Date fechaDeApertura,
+            boolean vuelosVip) {
+        this.nombre = nombre;
+        this.ciudadUbicacion = ciudadUbicacion;
+        this.cantidadDeVuelosAlDia = cantidadDeVuelosAlDia;
+        this.fechaDeApertura = fechaDeApertura;
+        this.vuelosVip = vuelosVip;
+    }
+    
     /**
-     * @param args the command line arguments
+     Este método aumenta el número de vuelos al día en 2000 y 
+     *posibilita los vuelos VIP
      */
+    
+    public void ventaDeTiques(){
+        this.cantidadDeVuelosAlDia += 2000;
+        this.vuelosVip = true;
+    }
+    
+    /**
+     Este método posbilita el cambio de la fecha de apertura después de
+     * la pandemia y cambiar el nombre del Aeropuerto
+     * @param nuevaFecha es la nueva fecha de apertura después de la pandemia
+     * @param nuevoNombre es el nuevo nombre del Aeropuerto
+     */
+    
+    public void reapertura(Date nuevaFecha, 
+            String nuevoNombre){
+            this.nombre = nuevoNombre;
+            this.fechaDeApertura = nuevaFecha;
+                    
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudadUbicacion() {
+        return ciudadUbicacion;
+    }
+
+    public void setCiudadUbicacion(String ciudadUbicacion) {
+        this.ciudadUbicacion = ciudadUbicacion;
+    }
+
+    public int getCantidadDeVuelosAlDia() {
+        return cantidadDeVuelosAlDia;
+    }
+
+    public void setCantidadDeVuelosAlDia(int cantidadDeVuelosAlDia) {
+        this.cantidadDeVuelosAlDia = cantidadDeVuelosAlDia;
+    }
+
+    public Date getFechaDeApertura() {
+        return fechaDeApertura;
+    }
+
+    public void setFechaDeApertura(Date fechaDeApertura) {
+        this.fechaDeApertura = fechaDeApertura;
+    }
+
+    public boolean getVuelosVip() {
+        return vuelosVip;
+    }
+
+    public void setVuelosVip(boolean vuelosVip) {
+        this.vuelosVip = vuelosVip;
+    }
+    
+    
+    
+    
+
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Aeropuerto aeropuertoinf = new Aeropuerto( "Airmarcat",
+                "Cali",3000,new Date(),true);
+        
+        Aeropuerto aeropuertoinf1 = new Aeropuerto("Catmarair",
+                "Bogota",5000,new Date(),false);
+        
+        System.out.println(aeropuertoinf.getNombre());
+        System.out.println(aeropuertoinf.getCantidadDeVuelosAlDia());
+        System.out.println(aeropuertoinf.getVuelosVip());
+        
+        aeropuertoinf.ventaDeTiques();
+        
+        System.out.println(aeropuertoinf.getNombre());
+        System.out.println(aeropuertoinf.getCantidadDeVuelosAlDia());
+        System.out.println(aeropuertoinf.getVuelosVip());
+        
     }
     
 }
